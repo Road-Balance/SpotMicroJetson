ebca8b6cbdca13b544cfb58d3572397b11bfdf1d
Ohdonghoon00 <ydyaum@gmail.com>
2020-09-26 15:04:10 +0900

    modify1

--- JetsonNano/test_servos_cali.py
+++ JetsonNano/test_servos_cali.py
@@ -35,21 +35,29 @@ print("Done initializing")
 #         kit.servo[0].angle=degree
 #         time.sleep(0.01)
 
+# [0]~[2] : 왼쪽 앞 다리 // [3]~[5] : 오른쪽 앞 다리 // [6]~[8] : 왼쪽 뒷 다리 // [9]~[11] : 오른쪽 뒷 다리
 val_list = [0, 180, 90, 180, 0, 90, \
             0, 180, 90, 180, 0, 90]
+
 
 for x in range(len(val_list)):
     kit.servo[x].angle = val_list[x]
 
 
 while True:
+    # num은 각도를 바꿀 모터 번호이다.
     num=int(input("Enter Servo to calibrate (0-12)"))
+    
     # print("Current Offset is {}".format(self.servos.getServoOffset(servo)))
+    
+    # offset은 위에서 정한 모터번호에 부여할 각도이다.
     offset=int(input("Enter new Offset:"))
      
     prev_offset = val_list[num]
+    
+    # prev_offset(현재각도)부터 offset(목표각도)까지 1도씩 증가 혹은 감소 
     sweep = range(prev_offset, offset, 1) if (prev_offset < offset) else range(prev_offset, offset, -1)
-    for degree in sweep :
+    for degree in sweep:
         kit.servo[num].angle=degree
         # kit.servo[1].angle=degree
         time.sleep(0.01)
