f217ce8fba4f92bc2ce37f81ff8bce157174ea7c
kimsooyoung <tge1375@naver.com>
2020-11-18 13:27:35 +0900

    lecture code

--- JetsonNano/examples/test_servos_cali.py
+++ JetsonNano/examples/test_servos_cali.py
@@ -16,37 +16,37 @@ import time
 print("Initializing Servos")
 i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
 print("Initializing ServoKit")
-kit = ServoKit(channels=16, i2c=i2c_bus0, address=0x40)
-# kit = ServoKit(channels=16, i2c=i2c_bus0, address=0x41)
-# kit[0] is the bottom servo
-# kit[1] is the top servo
+
+kit = list()
+kit.append(ServoKit(channels=16, i2c=i2c_bus0, address=0x40))
+# kit.append(ServoKit(channels=16, i2c=i2c_bus0, address=0x41))
+
+# kit[0] is the front servos
+# kit[1] is the rear servos
 print("Done initializing")
 
-# [0]~[2] : 왼쪽 앞 다리 // [3]~[5] : 오른쪽 앞 다리 // [6]~[8] : 왼쪽 뒷 다리 // [9]~[11] : 오른쪽 뒷 다리
-val_list = [0, 180, 90, 180, 0, 90, \
-            0, 180, 90, 180, 0, 90]
+# [0]~[2] : FL // [3]~[5] : FR // [6]~[8] : RL // [9]~[11] : RR
+val_list = [90]
+# val_list = [90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90]
 
 
-for x in range(len(val_list)):
-    kit.servo[x].angle = val_list[x]
 
+if __name__ == '__main__':
+    for x in range(len(val_list)):
+        kit[int(x/6)].servo[int(x%6)].angle = val_list[x]
 
-while True:
-    # num은 각도를 바꿀 모터 번호이다.
-    num=int(input("Enter Servo to calibrate (0-12)"))
-    
-    # print("Current Offset is {}".format(self.servos.getServoOffset(servo)))
-    
-    # offset은 위에서 정한 모터번호에 부여할 각도이다.
-    offset=int(input("Enter new Offset:"))
-     
-    prev_offset = val_list[num]
-    
-    # prev_offset(현재각도)부터 offset(목표각도)까지 1도씩 증가 혹은 감소 
-    sweep = range(prev_offset, offset, 1) if (prev_offset < offset) else range(prev_offset, offset, -1)
-    for degree in sweep:
-        kit.servo[num].angle=degree
-        # kit.servo[1].angle=degree
-        time.sleep(0.01)
+    while True:
+        # num is index of motor to rotate
+        num=int(input("Enter Servo to rotate (0-11): "))
+        
+        # new angle to be written on selected motor
+        angle=int(input("Enter new angles (0-180): "))
+        prev_angle = val_list[num]
+        
+        # increase(decrease) prev_angle to angle by 1 degree
+        sweep = range(prev_angle, angle, 1) if (prev_angle < angle) else range(prev_angle, angle, -1)
+        for degree in sweep:
+            kit[int(num/6)].servo[int(num%6)].angle=degree
+            time.sleep(0.01)
 
-    val_list[num] = offset
+        val_list[num] = angle
