78510ec7abcf2dd5dc040f351cdeae58e793a2e7
kimsooyoung <tge1375@naver.com>
2021-08-13 18:25:08 +0900

    Update for servokit

--- JetsonNano/examples/test_servos_cali.py
+++ JetsonNano/examples/test_servos_cali.py
@@ -11,9 +11,8 @@ print("Initializing Servos")
 i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
 print("Initializing ServoKit")
 
-kit = list()
-kit.append(ServoKit(channels=16, i2c=i2c_bus0, address=0x40))
-# kit.append(ServoKit(channels=16, i2c=i2c_bus0, address=0x41)) 
+kit = ServoKit(channels=16, i2c=i2c_bus0, address=0x40)
+kit2 = ServoKit(channels=16, i2c=i2c_bus0, address=0x41)
 
 # kit[0] is the front servos
 # kit[1] is the rear servos
@@ -21,26 +20,35 @@ print("Done initializing")
 
 # [0]~[2] : FL // [3]~[5] : FR // [6]~[8] : RL // [9]~[11] : RR
 val_list = [90]
+prev_angle = None
+cur_angle = None
 # val_list = [90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90]
 
 
-
 if __name__ == '__main__':
-    for x in range(len(val_list)):
-        kit[int(x/6)].servo[int(x%6)].angle = val_list[x]
 
     while True:
-        # num is index of motor to rotate
-        num=int(input("Enter Servo to rotate (0-11): "))
+        # motro_num is index of motor to rotate
+        motro_num=int(input("Enter Servo to rotate (0-11): "))
         
         # new angle to be written on selected motor
-        angle=int(input("Enter new angles (0-180): "))
-        prev_angle = val_list[num]
+        cur_angle=int(input("Enter new angles (0-180): "))
         
         # increase(decrease) prev_angle to angle by 1 degree
-        sweep = range(prev_angle, angle, 1) if (prev_angle < angle) else range(prev_angle, angle, -1)
-        for degree in sweep:
-            kit[int(num/6)].servo[int(num%6)].angle=degree
-            time.sleep(0.01)
+        if prev_angle:
+            sweep = range(prev_angle, cur_angle, 1) if (prev_angle < cur_angle) else range(prev_angle, cur_angle, -1)
 
-        val_list[num] = angle
+            for degree in sweep:
+                if motro_num < 6:
+                    kit.servo[int(motro_num%6)].angle = cur_angle
+                else:
+                    kit2.servo[int(motro_num%6)].angle = cur_angle
+                time.sleep(0.01)
+        else:
+            if motro_num < 6:
+                kit.servo[int(motro_num%6)].angle = cur_angle
+            else:
+                kit2.servo[int(motro_num%6)].angle = cur_angle
+
+
+        prev_angle = val_list[motro_num]
